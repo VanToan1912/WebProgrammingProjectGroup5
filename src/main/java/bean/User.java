80782a0d7889f265232aedf90323681fd70eb338
Le Thi Hanh Duyen <21130335@st.hcmuaf.edu.vn>
2024-01-12 15:17:31 +0700

    commit by eliza

---
+++ src/main/java/bean/User.java
@@ -0,0 +1,63 @@
+package bean;
+
+public class User {
+    private int user_id;
+    private String first_name;
+    private String last_name;
+    private String email;
+    private int telephone;
+    private String address;
+    private String city;
+
+    public int getUser_id() {
+        return user_id;
+    }
+    public void setUser_id(int user_id) {
+        this.user_id = user_id;
+    }
+    public String getFirst_name() {
+        return first_name;
+    }
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+    public String getLast_name() {
+        return last_name;
+    }
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public int getTelephone() {
+        return telephone;
+    }
+    public void setTelephone(int telephone) {
+        this.telephone = telephone;
+    }
+
+    public String getAddress_line2() {
+        return address;
+    }
+    public void setAddress_line2(String address_line2) {
+        this.address = address_line2;
+    }
+    public String getCity() {
+        return city;
+    }
+    public void setCity(String city) {
+        this.city = city;
+    }
+    @Override
+    public String toString() {
+        return "user [user_id=" + user_id + ", first_name=" + first_name + ", last_name=" + last_name + ", email="
+                + email + ", telephone=" + telephone + ", address_line1=" + address_line1 + ", address_line2="
+                + address_line2 + ", city=" + city + "]";
+    }
+
+
+}
